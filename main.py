2e3cc5056082700543fed3270c9bf1367c2c9766
Cris <cgutierrez.infor@gmail.com>
2023-06-26 16:47:40 -0400

    feat: new function to the sheet

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ function_descriptions = [
                 },
                 "category": {
                     "type": "string",
-                    "description": "Intenta categorizar este correo en categorías como las siguientes: Reportes, Inventario,Importaciones, Facturación; Soporte; Reunion con USA, etc."
+                    "description": "Intenta categorizar este correo en categorías como las siguientes: Reportes, Proyectos, Inventario ,Importaciones , Facturación, Soporte, USA, etc."
                 },
                 "tareas": {
                     "type": "string",
